b49dde61b16cf245bc9b30e2bb34739215c75315
Verda Bursal <verdabursal@Verdas-MacBook-Air.local>
2019-04-01 20:24:11 -0400

    add router structure

---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
+
+const App = () => {
+    <Router history={history}>
+        <Switch>
+
+        </Switch>
+    </Router>
+}

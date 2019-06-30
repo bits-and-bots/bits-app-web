519a0deaf7c79c424cb5d85d8f8fd7072065799a
rheisen <dennis.rheisen@gmail.com>
2019-06-30 16:20:27 -0400

    Add calendar event fetching to calendar page (rough)

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,6 +3,38 @@ import React from 'react';
 import '../styles/pages/calendar.scss';
 
 class Calendar extends React.Component {
+  componentDidMount = () => {
+    window.gapi.load('client', () => {
+      this.fetchEvents()
+    });
+  }
+
+  fetchEvents() {
+    window.gapi.client.init({
+      'apiKey': process.env.REACT_APP_GOOGLE_API_KEY
+    }).then(() => {
+      window.gapi.client.load("calendar", "v3", () => {
+        return window.gapi.client.calendar.events.list({
+          'calendarId': 'bitsandbots.neu@gmail.com',
+          'timeZone': new Date().getTimezoneOffset(),
+          'singleEvents': true,
+          'timeMin': (new Date()).toISOString(),
+          'maxResults': 20,
+          'orderBy': 'startTime'
+        }).then( (response) => {
+          let events = response.result.items
+          this.setState({
+            events
+          }, ()=>{
+            console.log(this.state.events);
+          })
+        }, function(reason) {
+          console.log(reason);
+        });
+      });
+    });
+  }
+
   render() {
     return (
       <div className="page-calendar__div__wrapper-page">

afb75cdad030cb5718774e263913265fcb17b4cb
rheisen <dennis.rheisen@gmail.com>
2019-06-30 14:05:06 -0400

    Improve routing, add mobile styles to home page splash

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PageLayout from './layouts/page-layout';
 
-const history = createHistory();
-
 const App = () => (
-  <Router history={history}>
+  <BrowserRouter>
     <Switch>
       <Route exact path='*' component={PageLayout}/>
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
 
 export default App;

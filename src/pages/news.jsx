a4e9ef77947c321204953e99bdd078497df0172a
Verda Bursal <verdabursal@Verdas-MacBook-Air.local>
2020-01-19 12:29:28 -0500

    added placeholder for news

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -9,6 +9,12 @@ class News extends React.Component {
         <header className="page-shared__header__container-splash-header">
           <h2 className="page-shared__h2__header-title"><span>Bits &amp; Bots</span> News</h2>
         </header>
+
+        <div className="page-news__div__container-text">
+          <p className="page-news__p__no-news">
+            No news yet, check back in later!
+          </p>
+        </div>
       </div>
     );
   }

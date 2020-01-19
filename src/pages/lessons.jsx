778c01b2e428403cfeb85aad8275e71b91b25559
Verda Bursal <verdabursal@Verdas-MacBook-Air.local>
2020-01-19 12:31:49 -0500

    added placeholder for lessons

--- src/pages/lessons.jsx
+++ src/pages/lessons.jsx
@@ -9,6 +9,12 @@ class Lessons extends React.Component {
         <header className="page-shared__header__container-splash-header">
           <h2 className="page-shared__h2__header-title"><span>Bits &amp; Bots</span> Lessons</h2>
         </header>
+
+        <div className="page-lessons__div__container-text">
+          <p className="page-lessons__p__no-lessons">
+            We'll be adding our lessons here shortly!
+          </p>
+        </div>
       </div>
     );
   }

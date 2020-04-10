bb5279dfbc36721807a840ced80036a7e4763764
Daniel Rassaby <drassaby@gmail.com>
2020-04-10 15:15:42 -0400

    Updates

---
+++ precache-manifest.ab047ca45a2a51afd75aacc651b63e36.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c44988538020e8a3a469",
+    "url": "/bits-app-web/static/css/main.789e9438.chunk.css"
+  },
+  {
+    "revision": "c44988538020e8a3a469",
+    "url": "/bits-app-web/static/js/main.d4703a03.chunk.js"
+  },
+  {
+    "revision": "f2425cb3a8fa083620e3",
+    "url": "/bits-app-web/static/js/runtime~main.e7e33796.js"
+  },
+  {
+    "revision": "29904c2aef7d64b2e48b",
+    "url": "/bits-app-web/static/js/2.88955b80.chunk.js"
+  },
+  {
+    "revision": "e004a45c506ffeee11e4ffd7e59a3041",
+    "url": "/bits-app-web/static/media/danny-headshot.e004a45c.jpeg"
+  },
+  {
+    "revision": "7e4ac2fea217351bc39960bb37d507cc",
+    "url": "/bits-app-web/static/media/bits-wordmark-small.7e4ac2fe.png"
+  },
+  {
+    "revision": "b41cdd93be8721903ee2e6c1e1e66452",
+    "url": "/bits-app-web/static/media/bits-wordmark-large.b41cdd93.png"
+  },
+  {
+    "revision": "6f8858509d2d8b8639c0749def1ec202",
+    "url": "/bits-app-web/static/media/verda-headshot.6f885850.JPG"
+  },
+  {
+    "revision": "4d572e168d615f4e816e82c247159909",
+    "url": "/bits-app-web/static/media/bits_and_bots_robot_sleuth.4d572e16.png"
+  },
+  {
+    "revision": "0c3a522d042d828396dd5dad3a015181",
+    "url": "/bits-app-web/index.html"
+  }
+];

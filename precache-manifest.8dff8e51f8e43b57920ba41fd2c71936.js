e82d6b649fa88b865c0f0dc6cef209c808e3a7e2
Daniel Rassaby <drassaby@gmail.com>
2020-04-25 00:04:59 -0400

    Updates

--- precache-manifest.8dff8e51f8e43b57920ba41fd2c71936.js
+++ precache-manifest.8dff8e51f8e43b57920ba41fd2c71936.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "1f57e8952f384ef6b28d",
+    "revision": "a1790e99bcc02c950feb",
     "url": "/bits-app-web/static/css/main.789e9438.chunk.css"
   },
   {
-    "revision": "1f57e8952f384ef6b28d",
-    "url": "/bits-app-web/static/js/main.2da91bfb.chunk.js"
+    "revision": "a1790e99bcc02c950feb",
+    "url": "/bits-app-web/static/js/main.33148a70.chunk.js"
   },
   {
     "revision": "f2425cb3a8fa083620e3",
     "url": "/bits-app-web/static/js/runtime~main.e7e33796.js"
   },
   {
-    "revision": "f676177e807c6b7abe7e",
-    "url": "/bits-app-web/static/js/2.c241f923.chunk.js"
+    "revision": "7e3db8b954dcaedcab48",
+    "url": "/bits-app-web/static/js/2.84bbde71.chunk.js"
   },
   {
     "revision": "7e4ac2fea217351bc39960bb37d507cc",
@@ -22,6 +22,10 @@ self.__precacheManifest = [
   {
     "revision": "b41cdd93be8721903ee2e6c1e1e66452",
     "url": "/bits-app-web/static/media/bits-wordmark-large.b41cdd93.png"
+  },
+  {
+    "revision": "6490cdf37e202944b3a51fa97561fa4a",
+    "url": "/bits-app-web/static/media/abbey_road.6490cdf3.jpg"
   },
   {
     "revision": "6f8858509d2d8b8639c0749def1ec202",
@@ -36,7 +40,7 @@ self.__precacheManifest = [
     "url": "/bits-app-web/static/media/bits_and_bots_robot_sleuth.4d572e16.png"
   },
   {
-    "revision": "306bc173b99bd83b0583e579294c3267",
+    "revision": "b6abb20e14b940f80c2e076febfa39cc",
     "url": "/bits-app-web/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bits-app-web/precache-manifest.478959b63fac101d0da77be97bd8a2f3.js"
+  "/bits-app-web/precache-manifest.8dff8e51f8e43b57920ba41fd2c71936.js"
 );
 
 workbox.clientsClaim();
